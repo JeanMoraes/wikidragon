34ce73ff7a0fb9049ff5fa85e52cd9d5c0d5a848
Jean Moraes <moraes.jean91@gmail.com>
2020-03-28 01:49:19 -0300

    construção dos estilos da tela Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,104 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import '../global.css';
+import './index.css';
+
+import logo from '../../assets/logo-wikidragon.png';
+
 function Home(){
     return(
-        <h1>LISTA DE DRAGÕES</h1>
+        <div className="container">
+            <aside>
+                <img src={logo} alt="WikiDragon" />
+
+                <h2>Menu</h2>
+                <nav>
+                    <button className="btn-nav">Todos os Dragões</button>
+                    <button className="btn-nav">Adicionar Dragão</button>
+                </nav>
+            </aside>
+            
+            <main>
+                <h1>LISTA DE DRAGÕES</h1>
+
+                <div className="container-dragon">
+                    
+                    <div className="box-dragon">
+                        <section>
+                            <p className="title">Nome do Dragão</p>
+                            <p className="info">Dragonite</p>
+                        </section>
+                        <section>
+                            <p className="title">Tipo</p>
+                            <p className="info">Fogo</p>
+                        </section>
+                        <section>
+                            <p className="title">Descrição</p>
+                            <p className="info">Breve descrição ...</p>
+                        </section>
+                        <section>
+                            <button className="btn-details">Ver Detalhes</button>
+                        </section>
+                    </div>
+
+                    <div className="box-dragon">
+                        <section>
+                            <p className="title">Nome do Dragão</p>
+                            <p className="info">Dragonite</p>
+                        </section>
+                        <section>
+                            <p className="title">Tipo</p>
+                            <p className="info">Fogo</p>
+                        </section>
+                        <section>
+                            <p className="title">Descrição</p>
+                            <p className="info">Breve descrição ...</p>
+                        </section>
+                        <section>
+                            <button className="btn-details">Ver Detalhes</button>
+                        </section>
+                    </div>
+
+                    <div className="box-dragon">
+                        <section>
+                            <p className="title">Nome do Dragão</p>
+                            <p className="info">Dragonite</p>
+                        </section>
+                        <section>
+                            <p className="title">Tipo</p>
+                            <p className="info">Fogo</p>
+                        </section>
+                        <section>
+                            <p className="title">Descrição</p>
+                            <p className="info">Breve descrição ...</p>
+                        </section>
+                        <section>
+                            <button className="btn-details">Ver Detalhes</button>
+                        </section>
+                    </div>
+
+                    <div className="box-dragon">
+                        <section>
+                            <p className="title">Nome do Dragão</p>
+                            <p className="info">Dragonite</p>
+                        </section>
+                        <section>
+                            <p className="title">Tipo</p>
+                            <p className="info">Fogo</p>
+                        </section>
+                        <section>
+                            <p className="title">Descrição</p>
+                            <p className="info">Breve descrição ...</p>
+                        </section>
+                        <section>
+                            <button className="btn-details">Ver Detalhes</button>
+                        </section>
+                    </div>
+
+                </div>
+            </main>
+        </div>
     );
 }
 
