4076a1c159c8ad202eec3b247dcc96f860296d90
Jean Moraes <moraes.jean91@gmail.com>
2020-03-27 23:07:23 -0300

    criando as primeiras rotas e componentes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from './components/Login/';
+import Home from './components/Home/';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/home" component={ Home } />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
