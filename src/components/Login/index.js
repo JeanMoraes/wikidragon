c101f7da58bf2094801ae4947a1615a5a116e662
Jean Moraes <moraes.jean91@gmail.com>
2020-03-28 00:22:15 -0300

    construção dos estilos da tela de Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import '../global.css';
+import './index.css';
+
+import logo from '../../assets/logo-wikidragon.png';
+
+
 function Login(){
     return(
-        <h1>Tela de Login</h1>
+        <div className="container box-login">
+            <div className="col col-l">
+                <img src={logo} alt="WikiDragon" className="logo-login" />
+            </div>
+            <div className="col">
+                <h1>Login</h1>
+                <p>A WikiDragon é uma enciclopédia on-line gratuita, criada e editada por voluntários em todo o mundo, que curtem dragões.</p>
+                <div className="form-login">
+                    <form>
+                        <input type="text" name="user" placeholder="Usuário" />
+                        <input type="pass" name="password" placeholder="Senha" />
+                        <button className="form-btn">ENTRAR</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
 
